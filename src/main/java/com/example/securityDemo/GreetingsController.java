44bd076a0a4bfd6336acbca534f8afa29b87e053
Pavan021 <pavanpanni008@gmail.com>
2026-06-12 01:58:24 +0530

    InMemory Auth

--- src/main/java/com/example/securityDemo/GreetingsController.java
+++ src/main/java/com/example/securityDemo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.securityDemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,19 @@ public class GreetingsController {
     public String sayHello()
     {
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminMethod()
+    {
+        return "Hello Admin!";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userMethod()
+    {
+        return "Hello User!";
     }
 }
--- src/main/java/com/example/securityDemo/SecurityConfig.java
+++ src/main/java/com/example/securityDemo/SecurityConfig.java
@@ -2,16 +2,23 @@ package com.example.securityDemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.core.userdetails.User.*;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -24,5 +31,19 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UserDetailsManager userDetailsManager()
+    {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user@1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin@1")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
 
 }
